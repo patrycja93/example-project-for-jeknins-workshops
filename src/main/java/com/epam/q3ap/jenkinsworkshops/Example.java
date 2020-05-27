333d79e7a4f6864e60eba28e7f5f69510d4499f1
patka <patrycja.hyjek@gmail.com>
2020-05-27 11:08:22 +0200

    Zmiana wyświetlanego napisu.

--- src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
+++ src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
@@ -9,6 +9,6 @@ package com.epam.q3ap.jenkinsworkshops;
  */
 class Example {
   public static void main(String[] args) {
-    System.out.println("Hello my dear world!");
+    System.out.println("Hello my dear world!!");
   }
 }
